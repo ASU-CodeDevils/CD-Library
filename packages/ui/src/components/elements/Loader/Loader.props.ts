85d2b659d1e3d6722705bedecad7d8f581b00b1c
Parker Eudey <parkereudey@gmail.com>
2022-03-10 20:02:51 -0600

    feat: added loader file outline

---
+++ packages/ui/src/components/elements/Loader/Loader.props.ts
@@ -0,0 +1,7 @@
+import { InputHTMLAttributes } from 'react';
+
+export interface LoaderProps extends InputHTMLAttributes<HTMLInputElement>{
+
+}
+
+export default LoaderProps;
---
+++ packages/ui/src/components/elements/Loader/Loader.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Loader(){
+    return(<div>Loader</div>);
+}
+
+export default Loader;

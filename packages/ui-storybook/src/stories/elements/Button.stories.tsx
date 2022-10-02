b256030fcc9fff31eaf0d26023176cb24a702322
frankjlin16 <flin30@asu.edu>
2022-10-01 23:49:43 -0700

    Add storybook page for button

---
+++ packages/ui-storybook/src/stories/elements/Button.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Button } from "@cd/ui";
+import "@cd/ui-styles/dist/cdui.css";
+
+export default {
+    title: "Elements/Button",
+    component: Button,
+    argTypes: {
+    },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {};

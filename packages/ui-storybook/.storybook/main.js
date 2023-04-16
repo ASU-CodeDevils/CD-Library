d6818bf354893c5e2e987c0a6498467e28016c04
Frankie <67761168+frankjlin16@users.noreply.github.com>
2023-04-16 02:52:43 -0700

    fix: update storybook

--- packages/ui-storybook/.storybook/main.js
+++ packages/ui-storybook/.storybook/main.js
@@ -1,12 +1,11 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "webpack5",
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
+  docs: {
+    autodocs: true
+  }
 };
